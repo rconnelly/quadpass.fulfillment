3d311aef3632b699da147abf5c12aa51c08e0b5d
Ryan Connelly <ryan.connelly@gmail.com>
2012-06-26 04:23:33 -0400

    Added pagination

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -3,25 +3,43 @@ var util = require('util')
 
 module.exports.setRoutes = function (app, kit) {
 
+    var yelpSearch = function (params, callback) {
+        var maxCount = 1000; // yelp imposed limit
+        var limit = params.limit || 18;
+        var page = params.page || 1;
+        var offset = (page - 1) * limit;
+        offset = offset <= (maxCount - limit) ? offset : maxCount - limit;
+        kit.yelp.search({term:params.term, location:params.location, limit:limit, offset:offset}, function (error, data) {
+            var total = (data.total > maxCount) ? maxCount : data.total
+            callback(error,
+                {
+                    searchResult:data,
+                    term:params.term,
+                    location:params.location,
+                    error:error,
+                    pageCount: Math.floor(total / limit),
+                    pageNumber:page
+                });
+        });
+    }
+
     app.get('/', function (req, res) {
         var p = {
-          term: req.session.term || 'Restaurants',
-          location: req.session.location || 'Atlanta, GA'
+            term:req.session.term || 'Restaurants',
+            location:req.session.location || 'Atlanta, GA'
         };
 
-        kit.yelp.search({term:p.term, location:p.location}, function (error, data) {
-            if (error) return next(error);
+        p.page = req.query.page;
 
-            res.render('index', {
-                page:{
-                    title:'QuadPass.com',
-                    className:'about'
-                },
-                searchResult:data,
-                term:p.term,
-                location:p.location,
-                error:error
-            });
+        yelpSearch(p, function (error, data) {
+            if (error) return next(JSON.stringify(error));
+
+            data.page = {
+                title:'QuadPass.com',
+                className:'about'
+            };
+
+            res.render('index', data);
         });
     });
 
@@ -40,28 +58,23 @@ module.exports.setRoutes = function (app, kit) {
         res.redirect('gifts-for');
     });
 
-
     app.get('/gifts-for/:term/:location', function (req, res, next) {
         var p = req.params;
-
         req.session.term = p.term;
         req.session.location = p.location;
+        p.page = req.query.page;
 
-        kit.yelp.search({term:p.term, location:p.location}, function (error, data) {
-            if (error) return next(error);
+        yelpSearch(p, function (error, data) {
 
-            res.render('index', {
-                page:{
-                    title:'QuadPass.com',
-                    className:'about'
-                },
-                searchResult:data,
-                term:p.term,
-                location:p.location,
-                error:error
-            });
+            if (error) return next(JSON.stringify(error));
+
+            data.page = {
+                title:'QuadPass.com - Order Gift Page',
+                className:'orderpage'
+            };
+
+            res.render('index', data);
         });
-
 
     });
 
